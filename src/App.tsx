f41d803793fa9e522d88de137736b73a4e3ac32d
Daniel Eicher <d.eicher@mailbox.org>
2022-04-11 17:36:05 +0200

    Add submit form and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
-      <span className="heading">Taskify</span>
+      <h1 className="heading">Taskify</h1>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map(t => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
